c0ae5ab9f507786efbdcee595466b4ceb1bfe316
AKV85 <a.kotov.laknojus@gmail.com>
2023-04-18 00:36:12 +0300

    Understanding the useState Hook2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import { useState } from "react";
 
 function App({ library }) {
-  const [what] = useState("happy");
-  console.log(what);
+  const [emotion, setEmotion] = useState("happy");
+  
   return (
     <div className="App">
       <h1>Hello, {library} is here, baby!</h1>
+      <h2>Current emotion is {emotion}</h2>
     </div>
   );
 }
