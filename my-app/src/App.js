0a1458861e72008c495a349d6227916dd222f699
AKV85 <a.kotov.laknojus@gmail.com>
2023-04-18 00:10:47 +0300

    start learning.React.StrictMode2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 
-function App(props) {
+function App({library}) {
   return (
     <div className="App">
-      <h1>Hello, {props.library} is here, baby!</h1>
+      <h1>Hello, {library} is here, baby!</h1>
     </div>
   );
 }
