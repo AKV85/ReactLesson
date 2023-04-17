098c0b8878b496e1f914bfeb6eaa1013e2a03bea
AKV85 <a.kotov.laknojus@gmail.com>
2023-04-18 00:15:36 +0300

    console.log

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 
-function App({library}) {
+const cities = ["Tokyo", "Chicago", "London"];
+
+console.log(cities[0,1]);
+
+function App(props) {
   return (
     <div className="App">
-      <h1>Hello, {library} is here, baby!</h1>
+      <h1>Hello, {props.library} is here, baby!</h1>
     </div>
   );
 }
