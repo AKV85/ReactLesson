8d3ba23072528c174def7e78ecc4dd1f636575f8
AKV85 <a.kotov.laknojus@gmail.com>
2023-04-18 15:33:21 +0300

    Handling loading states

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,22 +15,31 @@ return(
 
 function App() {
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
+    setLoading(true);
     fetch(
       `https://api.github.com/users/akv85`
     )
       .then((response) => response.json()) 
-      .then(setData);
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
   }, []);
-  if(data)
+
+  if(loading) return <h1>Loading...</h1>;
+  if(error) return <pre>{JSON.stringify(error)}</pre>;
+  if(!data) return null;
+
    return (
    <GithubUser 
       login={data.login} 
       type={data.type}
       avatar={data.avatar_url} 
-  />
+    />
    );
-  return <h1>Data</h1>;
 }
 
 export default App;
